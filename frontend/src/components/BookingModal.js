5cb7c3f8e372ce7d7c453a50fb4e450019b78e06
arch-124 <149924672+arch-124@users.noreply.github.com>
2025-11-07 23:16:20 +0530

    BookingModal.js

--- frontend/src/components/BookingModal.js
+++ frontend/src/components/BookingModal.js
@@ -16,7 +16,7 @@ export default function BookingModal({ service, onClose }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/bookings/create", {
+    const res = await fetch("https://glra-newback.onrender.com/api/bookings/create", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ...formData, service })
